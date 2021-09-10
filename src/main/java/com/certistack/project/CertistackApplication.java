6f243b980f8d179d5a435538e12c53fda60a9ce3
rodrigo <rothbarth@logikee.com.br>
2021-09-10 18:21:07 -0300

    first version

---
+++ src/main/java/com/certistack/project/CertistackApplication.java
@@ -0,0 +1,13 @@
+package com.certistack.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertistackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertistackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/certistack/project/CertistackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certistack.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertistackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
